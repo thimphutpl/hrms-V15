a4ff65efaf992be2c442202745653858fa101670
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2025-04-03 15:00:48 +0600

    benefit non

--- hrms/hr/doctype/employee_benefit_type/employee_benefit_type.js
+++ hrms/hr/doctype/employee_benefit_type/employee_benefit_type.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Benefit Type", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Benefit Type", {
+    setup(frm) {
+        frm.set_query("account", "accounts", function(doc, cdt, cdn) {
+			var d = locals[cdt][cdn];
+			return {
+				filters: {
+					"is_group": 0,
+					"company": d.company
+				}
+			};
+		});
+    },
+    
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
