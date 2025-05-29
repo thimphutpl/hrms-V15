1663263cc3547170b309b9c5e45196ac9a9b3153
Karma <ktenzleo@gmail.com>
2025-05-29 10:20:44 +0600

    updated dfg unfreeze

--- hrms/hr/doctype/dfg_and_gfg/dfg_and_gfg.js
+++ hrms/hr/doctype/dfg_and_gfg/dfg_and_gfg.js
@@ -1,10 +1,22 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DFG AND GFG', {
+frappe.ui.form.on('DFG AND GFG', {	
 	refresh: function(frm) {
-		cur_frm.toggle_reqd("date_of_separation", frm.doc.status == "Left")
+		cur_frm.toggle_reqd("date_of_separation", frm.doc.status == "Left");
 
+		// Add Unfreeze Operator button if status is Left and not blacklisted
+		if (frm.doc.status === "Left" && frm.doc.black_listed != 1) {
+			frm.add_custom_button(__("Unfreeze DFG/GFG"), () => {
+				frappe.call({
+					method: "hrms.hr.doctype.dfg_and_gfg.dfg_and_gfg.rejoin_dfg_gfg",
+					args: { docname: frm.doc.name },
+					callback: () => {
+						window.location.reload();
+					}
+				});
+			});
+		}
 	},
 	onload: function(frm) {
 		if(!frm.doc.date_of_joining) {
@@ -14,44 +26,34 @@ frappe.ui.form.on('DFG AND GFG', {
 	"status": function(frm) {
 		cur_frm.toggle_reqd("date_of_separation", frm.doc.status == "Left")
 	},
-// 	branch: function(frm){
-// 		if(frm.doc.branch){
-// 				frappe.call({
-// 						method: 'frappe.client.get_value',
-// 						args: {
-// 								doctype: 'Cost Center',
-// 								filters: {
-// 										'branch': frm.doc.branch,
-// 										'is_group': 0
-// 								},
-// 								fieldname: ['name']
-// 						},
-// 						callback: function(r){
-// 								if(r.message){
-// 										cur_frm.set_value("cost_center", r.message.name);
-// 										refresh_field('cost_center');
-// 								}
-// 						}
-// 				});
-// 		}
-// },
-cost_center: function(frm){
-	if(!frm.doc.__islocal){
-		cur_frm.set_value("date_of_transfer",frappe.datetime.nowdate());
-		refresh_many(["date_of_transfer"]);
-		validate_prev_doc(frm,__("Please select date of transfer to new cost center"));		
-	}
-},
+	branch: function(frm) {
+		frm.__cost_center_autofetch = true;
+	},
+	cost_center: function(frm){
+		if (frm.__cost_center_autofetch) {			
+			frm.__cost_center_autofetch = false;
+			return;
+		}
+		if (!frm.__cost_center_checked) {
+			frm.__cost_center_checked = true;
+			if(!frm.doc.__islocal){
+				cur_frm.set_value("date_of_transfer",frappe.datetime.nowdate());
+				refresh_many(["date_of_transfer"]);
+				validate_prev_doc(frm,__("Please select date of transfer to new cost center"));		
+			}			
+			setTimeout(() => { frm.__cost_center_checked = false; }, 1000);
+		}
+	},
 });
 frappe.ui.form.on("DFG AND GFG", "refresh", function(frm) {
-    cur_frm.set_query("cost_center", function() {
-        return {
-            "filters": {
+	cur_frm.set_query("cost_center", function() {
+		return {
+			"filters": {
 		"is_group": 0,
 		"is_disabled": 0
-            }
-        };
-    });
+			}
+		};
+	});
 })
 function validate_prev_doc(frm, title){
 	return frappe.call({
--- hrms/hr/doctype/dfg_and_gfg/dfg_and_gfg.py
+++ hrms/hr/doctype/dfg_and_gfg/dfg_and_gfg.py
@@ -62,65 +62,70 @@ class DFGANDGFG(Document):
 				black_listed = frappe.db.get_value("DFG AND GFG", block_listed_transaction, "black_listed")
 				if black_listed:
 					frappe.throw("This Desuup OR Gyalsup has been Black listed")
-	def populate_work_history(self):
-		if len(self.internal_work_history) == 0:
+	
+	# Following method introducted by SHIV on 04/10/2017
+	def populate_work_history(self):		
+		if self.is_new() or len(self.internal_work_history) == 0:
 			self.append("internal_work_history",{
-				"branch": self.branch,
-				"cost_center": self.cost_center,
-				"from_date": self.date_of_joining,
-				"owner": frappe.session.user,
-				"creation": nowdate(),
-				"modified_by": frappe.session.user,
-				"modified": nowdate()
-			})
-		else:                        
-			# Fetching previous document from db
-			if frappe.db.exists(self.doctype, self.name):	
-				prev_doc = frappe.get_doc(self.doctype,self.name)
-				self.date_of_transfer = self.date_of_transfer if self.date_of_transfer else today()
-				
-				if (getdate(self.date_of_joining) != prev_doc.date_of_joining) or \
-				(self.status == 'Left' and self.date_of_separation) or \
-				(self.cost_center != prev_doc.cost_center):
-					for wh in self.internal_work_history:
-						# For change in date_of_joining
-						if (getdate(self.date_of_joining) != prev_doc.date_of_joining):
-							if (getdate(prev_doc.date_of_joining) == getdate(wh.from_date)):
-								wh.from_date = self.date_of_joining
-
-						# For change in date_of_separation, cost_center
-						if (self.status == 'Left' and self.date_of_separation):
-							if not wh.to_date:
-								wh.to_date = self.date_of_separation
-							elif prev_doc.date_of_separation:
-								if (getdate(prev_doc.date_of_separation) == getdate(wh.to_date)):
-									wh.to_date = self.date_of_separation
-						elif (self.cost_center != prev_doc.cost_center):
-							if getdate(self.date_of_transfer) > getdate(today()):
-								frappe.throw(_("Date of transfer cannot be a future date."),title="Invalid Date")      
-							elif not wh.to_date:
-								if getdate(self.date_of_transfer) < getdate(wh.from_date):
-									frappe.throw(_("Row#{0} : Date of transfer({1}) cannot be beyond current effective entry.").format(wh.idx,self.date_of_transfer),title="Invalid Date")
-									
-								wh.to_date = wh.from_date if add_days(getdate(self.date_of_transfer),-1) < getdate(wh.from_date) else add_days(self.date_of_transfer,-1)
+						"branch": self.branch,
+						"cost_center": self.cost_center,
+						"from_date": self.date_of_joining,
+						"owner": frappe.session.user,
+						"creation": nowdate(),
+						"modified_by": frappe.session.user,
+						"modified": nowdate(),
 						
+			})
+		else:
+			# Fetching previous document from db
+			prev_doc = frappe.get_doc(self.doctype,self.name)
+			self.date_of_transfer = self.date_of_transfer if self.date_of_transfer else today()
+			if (getdate(self.date_of_joining) != prev_doc.date_of_joining) or \
+			   (self.status == 'Left' and self.date_of_separation) or \
+			   (self.cost_center != prev_doc.cost_center):
+				for idx, wh in enumerate(self.internal_work_history):
+					if (getdate(self.date_of_joining) != prev_doc.date_of_joining):
+						if (
+							idx == len(self.internal_work_history) - 1 and
+							idx != 0 and
+							wh.to_date is None and
+							(getdate(prev_doc.date_of_joining) == getdate(wh.from_date)) and
+							all(getdate(wh2.from_date) != getdate(self.date_of_joining) for i2, wh2 in enumerate(self.internal_work_history) if i2 < idx and i2 != 0)
+						):
+							wh.from_date = self.date_of_joining
+					if (self.status == 'Left' and self.date_of_separation):
+						if not wh.to_date:
+							wh.to_date = self.date_of_separation
+							if wh.from_date > wh.to_date:
+								frappe.throw("To date cannot be before From Date (Separation Date)")
+					elif prev_doc.date_of_separation:
+						if (getdate(prev_doc.date_of_separation) == getdate(wh.to_date)):
+							wh.to_date = self.date_of_separation
 				if (self.cost_center != prev_doc.cost_center):
-					self.append("internal_work_history",{
-							"branch": self.branch,
-							"cost_center": self.cost_center,
-							"from_date": self.date_of_transfer,
-							"owner": frappe.session.user,
-							"creation": nowdate(),
-							"modified_by": frappe.session.user,
-							"modified": nowdate()
-					})
-				elif not self.internal_work_history:
-					self.append("internal_work_history",{
-								"branch": self.branch,
-								"cost_center": self.cost_center,
-								"from_date": self.date_of_joining,
-								"owner": frappe.session.user,
-								"creation": nowdate(),
-								"modified_by": frappe.session.user,
-								"modified": nowdate()
-					})
+					if getdate(self.date_of_transfer) > getdate(today()):
+						frappe.throw(_("Date of transfer cannot be a future date."),title="Invalid Date")      
+					elif not wh.to_date:
+						if getdate(self.date_of_transfer) < getdate(wh.from_date):
+							frappe.throw(_("Row#{0} : Date of transfer({1}) cannot be beyond current effective entry.").format(wh.idx,self.date_of_transfer),title="Invalid Date")
+						wh.to_date = wh.from_date if add_days(getdate(self.date_of_transfer),-1) < getdate(wh.from_date) else add_days(self.date_of_transfer,-1)
+		if 'prev_doc' in locals() and ((self.cost_center != prev_doc.cost_center) or (prev_doc.status == 'Left' and self.status == 'Active')):
+			self.append("internal_work_history",{
+					"branch": self.branch,
+					"cost_center": self.cost_center,
+					"from_date": self.date_of_transfer,
+					"owner": frappe.session.user,
+					"creation": nowdate(),
+					"modified_by": frappe.session.user,
+					"modified": nowdate(),
+					
+			})	
+
+@frappe.whitelist()
+def rejoin_dfg_gfg(docname):
+	doc = frappe.get_doc("DFG AND GFG", docname)
+	if doc.docstatus != 1 or doc.status != "Left":
+		frappe.throw("Only submitted and 'Left' employees can be rejoined.")
+	frappe.db.sql("""
+		update `tabDFG AND GFG` set docstatus=0, status='Active', date_of_separation=NULL where name=%s
+	""", (docname,))
+	return "Employee rejoined, status set to Active and docstatus set to Draft."
