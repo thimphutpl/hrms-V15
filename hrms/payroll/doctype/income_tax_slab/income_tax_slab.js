fd5ed70bed272dcb0bd8b11fca9e42c791bd4d3e
kinleydorji <kd77144@gmail.com>
2025-06-19 10:51:05 +0600

    income tax slab

--- hrms/payroll/doctype/income_tax_slab/income_tax_slab.js
+++ hrms/payroll/doctype/income_tax_slab/income_tax_slab.js
@@ -4,5 +4,11 @@
 frappe.ui.form.on('Income Tax Slab', {
 	currency: function(frm) {
 		frm.refresh_fields();
-	}
+	},
+	onload_post_render: function(frm){
+		if (frm.doc.docstatus === 1){
+			$(".grid-footer").attr('style','');
+		}
+		$(".grid-upload").addClass('hidden');
+	},
 });
