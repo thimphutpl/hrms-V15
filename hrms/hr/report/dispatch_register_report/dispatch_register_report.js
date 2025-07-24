e90f83ed0e54cd815147bcf7b456f6b5db7f8a73
Sonam-555 <sonamdoji384@gmail.com>
2025-07-24 15:45:57 +0600

    Update dispatch_register_report

--- hrms/hr/report/dispatch_register_report/dispatch_register_report.js
+++ hrms/hr/report/dispatch_register_report/dispatch_register_report.js
@@ -3,6 +3,16 @@
 
 frappe.query_reports["Dispatch Register Report"] = {
 	"filters": [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+		},
 
 	]
 };
--- hrms/hr/report/dispatch_register_report/dispatch_register_report.py
+++ hrms/hr/report/dispatch_register_report/dispatch_register_report.py
@@ -5,19 +5,31 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
+	if not filters:
+		filters = {}
 	columns, data = get_columns(filters), get_data(filters)
 	return columns, data
 
 def get_data(filters=None):
-    data=frappe.db.sql("select date, company, department, subject, file_no, place, remarks from `tabDispatch Register` where 1=1", as_dict=True)
-    # frappe.throw(str(data))
-    return data
+	if filters.get("to_date") or filters.get("from_date"):
+		condition=get_conditions(filters)
+		data=frappe.db.sql("select date, to_whom_sent, subject, file_no, place, remarks from `tabDispatch Register` where 1=1"+condition, as_dict=True)
+	else:
+		data=frappe.db.sql("select date, to_whom_sent, subject, file_no, place, remarks from `tabDispatch Register` where 1=1 order by date desc", as_dict=True)
+	# frappe.throw(str(data))
+	return data
 
-# def get_conditions(filters=None):
-    
-
+def get_conditions(filters=None):
+	condition=""
+	if filters.get("to_date"):
+		condition+=" and date<='{}'".format(filters.get("to_date"))
+	if filters.get("from_date"):
+		condition+=" and date>='{}'".format(filters.get("from_date"))
+	condition+=" order by date desc"
+	return condition
+	
 def get_columns(filters=None):
-    return [
+	return [
 		{
 			"label": _("Date"),
 			"fieldname": "date",
@@ -25,18 +37,10 @@ def get_columns(filters=None):
 			"width": 140,
 		},
 		{
-			"label": _("Company"),
-			"fieldname": "company",
-			"fieldtype": "Link",
-			"options": "Company",
+			"label": _("To Whom Sent"),
+			"fieldname": "to_whom_sent",
+			"fieldtype": "Data",
 			"width": 180,
-		},
-		{
-			"label": _("Department"),
-			"fieldname": "department",
-			"fieldtype": "Link",
-			"options":"Department",
-			"width": 240,
 		},
 		{
 			"label": _("Subject"),
@@ -51,7 +55,7 @@ def get_columns(filters=None):
 			"width": 140,
 		},
 		{
-			"label": _("Place"),
+			"label": _("Name"),
 			"fieldname": "place",
 			"fieldtype": "Data",
 			"width": 150,
