d70f541f911d5e159b3a7931296ba945d12781ad
sherubrangdrel <sherubrangrel@thimphutechpark.bt>
2025-09-13 13:14:43 +0600

    hrms ltc relation.json ,trvl auth and its dashborad

--- hrms/hr/doctype/leave_travel_concession/leave_travel_concession.py
+++ hrms/hr/doctype/leave_travel_concession/leave_travel_concession.py
@@ -18,14 +18,26 @@ class LeaveTravelConcession(Document):
 
 	def on_submit(self):
 		
-		for a in self.items:
-			#cost_center = frappe.db.get_value("Employee", a.employee, ["cost_center"])
-			tax=a.tax
-			basic_pay=a.basic_pay
-			net_amt=a.amount
+		# for a in self.items:
+			
+		# 	employee=a.employee
+		# 	tax=a.tax
+		# 	basic_pay=a.basic_pay
+		# 	net_amt=a.amount
 			
 				
-		self.post_journal_entry(tax,basic_pay,net_amt)
+		# self.post_journal_entry(employee,tax,basic_pay,net_amt)
+		employee_data = []
+		for a in self.items:
+			employee_data.append({
+				'employee': a.employee,
+				'tax': a.tax,
+				'basic_pay': a.basic_pay,
+				'net_amt': a.amount
+			})
+
+		# Process all data at once
+		self.post_journal_entry(employee_data)
 	def validate_employee(self):
 		if self.employee:
 			employment_type = frappe.db.get_value("Employee",self.employee,"employment_status")
@@ -61,8 +73,9 @@ class LeaveTravelConcession(Document):
 		else:
 			frappe.throw("Cannot save without any employee records")
 
-	def post_journal_entry(self,tax,basic_pay,net_amt):
+	#def post_journal_entry(self,employee,tax,basic_pay,net_amt):
 	#def post_journal_entry(self):
+	def post_journal_entry(self, employee_data):
 		ltc_expense_account 	= frappe.db.get_single_value("HR Accounts Settings","ltc_account")
 		ltc_payable_account 	= frappe.db.get_single_value("HR Accounts Settings","ltc_payable")
 		tax_account 				= frappe.db.get_value("Company", self.company, "default_salary_tax_account")
@@ -102,143 +115,264 @@ class LeaveTravelConcession(Document):
 			)
 
 		posting = frappe._dict()
-		
-		# Payables
-		posting.setdefault("to_payables", []).append({
-			"account" 					: ltc_expense_account,
-			"debit_in_account_currency"	: basic_pay,
-			# "cost_center"    			: self.cost_center,
-			"party_check"			 	: 0,
-			"reference_type"			: self.doctype,
-			"reference_name"			: self.name,
-		})
-		if flt(tax) > 0:
+		for data in employee_data:
+			employee = data['employee']
+			tax = data['tax']
+			basic_pay = data['basic_pay']
+			net_amt = data['net_amt']
+
+			# Payables entries for each employee
 			posting.setdefault("to_payables", []).append({
-				"account" 					: tax_account,
-				"credit_in_account_currency": flt(tax),
-				# "cost_center"    			: self.cost_center,
-				"party_check"				: 0,
-				"reference_type"			: self.doctype,
-				"reference_name"			: self.name,
+				"account": ltc_expense_account,
+				"debit_in_account_currency": basic_pay,
+				"party_check": 0,
+				"reference_type": self.doctype,
+				"reference_name": self.name,
 			})
-		posting.setdefault("to_payables", []).append({
-			"account" 						: ltc_payable_account,
-			"credit_in_account_currency"	: net_amt,
-			# "cost_center"    				: self.cost_center,
-			"party_check"					: 1,
-			"party_type"					: "Employee",
-			"party"							: self.employee,
-			"reference_type"				: self.doctype,
-			"reference_name"				: self.name,
-		})
+			
+			if flt(tax) > 0:
+				posting.setdefault("to_payables", []).append({
+					"account": tax_account,
+					"credit_in_account_currency": flt(tax),
+					"party_check": 0,
+					"reference_type": self.doctype,
+					"reference_name": self.name,
+				})
+				
+			posting.setdefault("to_payables", []).append({
+				"account": ltc_payable_account,
+				"credit_in_account_currency": net_amt,
+				"party_check": 1,
+				"party_type": "Employee",
+				"party": employee,
+				"reference_type": self.doctype,
+				"reference_name": self.name,
+			})
 
-		# To Bank
-		posting.setdefault("to_bank", []).append({
-			"account"       				: ltc_payable_account,
-			"debit_in_account_currency"		: net_amt,
-			# "cost_center"   				: self.cost_center,
-			"party_check"					: 1,
-			"party_type"					: "Employee",
-			"party"							: self.employee,
-			"reference_type"				: self.doctype,
-			"reference_name"				: self.name,
-		})
-		posting.setdefault("to_bank", []).append({
-			"account"       				: default_bank_account,
-			"credit_in_account_currency"	: net_amt,
-			# "cost_center"   				: self.cost_center,
-			"party_check"   				: 0,
-			"reference_type"				: self.doctype,
-			"reference_name"				: self.name,
-		})
+			# Bank entries for each employee
+			posting.setdefault("to_bank", []).append({
+				"account": ltc_payable_account,
+				"debit_in_account_currency": net_amt,
+				"party_check": 1,
+				"party_type": "Employee",
+				"party": employee,
+				"reference_type": self.doctype,
+				"reference_name": self.name,
+			})
+			
+			posting.setdefault("to_bank", []).append({
+				"account": default_bank_account,
+				"credit_in_account_currency": net_amt,
+				"party_check": 0,
+				"reference_type": self.doctype,
+				"reference_name": self.name,
+			})
 
+		# Create journal entries
 		jv_name, v_title = None, ""
 		for i in posting:
 			if i == "to_payables":
-				title         = "To Payables"
-				voucher_type  = "Journal Entry"
+				title = "To Payables"
+				voucher_type = "Journal Entry"
 				naming_series = "Journal Voucher"
 			else:
-				title         = "To Bank"
-				voucher_type  = "Bank Entry"
+				title = "To Bank"
+				voucher_type = "Bank Entry"
 				naming_series = "Bank Payment Voucher"
 
 			doc = frappe.get_doc({
-					"doctype"			: "Journal Entry",
-					"voucher_type"		: voucher_type,
-					"naming_series"		: naming_series,
-					"title"				: title,
-					"remark"			: title,
-					"posting_date"		: nowdate(),                     
-					"company"			: self.company,
-					"accounts"			: posting[i],
-					"branch"			: self.branch,
-				})
+				"doctype": "Journal Entry",
+				"voucher_type": voucher_type,
+				"naming_series": naming_series,
+				"title": title,
+				"remark": title,
+				"posting_date": nowdate(),                     
+				"company": self.company,
+				"accounts": posting[i],
+				"branch": self.branch,
+			})
 
-			
 			doc.flags.ignore_permissions = 1 
 			doc.insert()
 			if i == "to_payables":
 				doc.submit()
-			else:
-				pass
+
+		
+		# Payables
+		# posting.setdefault("to_payables", []).append({
+		# 	"account" 					: ltc_expense_account,
+		# 	"debit_in_account_currency"	: basic_pay,
+		# 	# "cost_center"    			: self.cost_center,
+		# 	"party_check"			 	: 0,
+		# 	"reference_type"			: self.doctype,
+		# 	"reference_name"			: self.name,
+		# })
+		# if flt(tax) > 0:
+		# 	posting.setdefault("to_payables", []).append({
+		# 		"account" 					: tax_account,
+		# 		"credit_in_account_currency": flt(tax),
+		# 		# "cost_center"    			: self.cost_center,
+		# 		"party_check"				: 0,
+		# 		"reference_type"			: self.doctype,
+		# 		"reference_name"			: self.name,
+		# 	})
+		# posting.setdefault("to_payables", []).append({
+		# 	"account" 						: ltc_payable_account,
+		# 	"credit_in_account_currency"	: net_amt,
+		# 	# "cost_center"    				: self.cost_center,
+		# 	"party_check"					: 1,
+		# 	"party_type"					: "Employee",
+		# 	"party"							: employee,
+		# 	"reference_type"				: self.doctype,
+		# 	"reference_name"				: self.name,
+		# })
+
+		# # To Bank
+		# posting.setdefault("to_bank", []).append({
+		# 	"account"       				: ltc_payable_account,
+		# 	"debit_in_account_currency"		: net_amt,
+		# 	# "cost_center"   				: self.cost_center,
+		# 	"party_check"					: 1,
+		# 	"party_type"					: "Employee",
+		# 	"party"							: employee,
+		# 	"reference_type"				: self.doctype,
+		# 	"reference_name"				: self.name,
+		# })
+		# posting.setdefault("to_bank", []).append({
+		# 	"account"       				: default_bank_account,
+		# 	"credit_in_account_currency"	: net_amt,
+		# 	# "cost_center"   				: self.cost_center,
+		# 	"party_check"   				: 0,
+		# 	"reference_type"				: self.doctype,
+		# 	"reference_name"				: self.name,
+		# })
+
+		# jv_name, v_title = None, ""
+		# for i in posting:
+		# 	if i == "to_payables":
+		# 		title         = "To Payables"
+		# 		voucher_type  = "Journal Entry"
+		# 		naming_series = "Journal Voucher"
+		# 	else:
+		# 		title         = "To Bank"
+		# 		voucher_type  = "Bank Entry"
+		# 		naming_series = "Bank Payment Voucher"
+
+		# 	doc = frappe.get_doc({
+		# 			"doctype"			: "Journal Entry",
+		# 			"voucher_type"		: voucher_type,
+		# 			"naming_series"		: naming_series,
+		# 			"title"				: title,
+		# 			"remark"			: title,
+		# 			"posting_date"		: nowdate(),                     
+		# 			"company"			: self.company,
+		# 			"accounts"			: posting[i],
+		# 			"branch"			: self.branch,
+		# 		})
+
+			
+		# 	doc.flags.ignore_permissions = 1 
+		# 	doc.insert()
+		# 	if i == "to_payables":
+		# 		doc.submit()
+		# 	else:
+		# 		pass
 				#self.db_set("journal_entry", doc.name)
 				#self.db_set("journal_entry_status", "Forwarded to accounts for processing payment on ")
 
 		
 
 	def on_cancel(self):
+		
 		jv_doc = frappe.get_doc("Journal Entry", self.journal_entry)
 		# jv = frappe.db.get_value("Journal Entry", self.journal_entry, "docstatus")
 		if jv_doc and jv_doc.docstatus != 2:
+			
 			frappe.throw("Can not cancel LTC without canceling the corresponding journal entry " + str(self.journal_entry))
 		else:
+			
 			self.db_set("journal_entry", None)
 
 
 	@frappe.whitelist()
 	def get_ltc_details(self):
-		start, end = frappe.db.get_value("Fiscal Year", self.fiscal_year, ["year_start_date", "year_end_date"])
-		employee_filter = ""
-		if self.employee:
-			employee_filter = " and b.employee = '{}' ".format(self.employee)
-		entries = frappe.db.sql("""
-					select 
-						e.date_of_joining, 
-						b.employee, 
-						b.employee_name, 
-						b.branch, 
-						a.amount, 
-						e.bank_name, 
-						e.bank_ac_no  
-					from 
-						`tabSalary Detail` a, 
-						`tabSalary Structure` b, 
-						`tabEmployee` e 
-					where 
-						a.parent = b.name 
-						and b.employee = e.name 
-						and a.salary_component = 'Basic Salary' 
-						and (b.is_active = 'Yes' or e.relieving_date between \'"+str(start)+"\' and \'"+str(end)+"\')
-						and b.eligible_for_ltc = 1
-						""" + employee_filter + """
-					order by b.branch """, as_dict=True)
-		self.set('items', [])
-		for d in entries:
-			datediff = date_diff(nowdate(), d.date_of_joining)
-			# frappe.throw(str(datediff))
-			if datediff < 366:
-				frappe.throw("your not engiable for ltc")
-				
+		try:
+		
+			start, end = frappe.db.get_value(
+			"Fiscal Year", 
+			self.fiscal_year, 
+			["year_start_date", "year_end_date"]
+			)
 
-			else:
-				
-				d.tax = get_salary_tax(d.amount)
-				d.basic_pay=d.amount
-				d.amount=d.amount-d.tax
-				
-			row = self.append('items', {})
 			
-			row.update(d)
+			employee_filter = ""
+			if self.employee:
+				employee_filter = f" AND b.employee = {frappe.db.escape(self.employee)}"
 
+			
+			if not self.items:
+				frappe.msgprint("No employees selected in items")
+				return
+
+			emp_list = ", ".join([frappe.db.escape(a.employee) for a in self.items])
+
+			
+			query = f"""
+			SELECT 
+			e.date_of_joining, 
+			b.employee, 
+			b.employee_name, 
+			b.branch, 
+			a.amount, 
+			e.bank_name, 
+			e.bank_ac_no  
+			FROM 
+			`tabSalary Detail` a
+			INNER JOIN 
+			`tabSalary Structure` b ON a.parent = b.name
+			INNER JOIN 
+			`tabEmployee` e ON b.employee = e.name
+			WHERE 
+			a.salary_component = 'Basic Salary' 
+			AND (b.is_active = 'Yes' OR e.relieving_date BETWEEN %s AND %s)
+			AND b.eligible_for_ltc = 1 
+			AND DATEDIFF(CURDATE(), e.date_of_joining) >= 366
+			{employee_filter}
+			and NOT EXISTS (
+                SELECT 1 
+                FROM `tabLeave Travel Concession` existing_ltc
+                INNER JOIN `tabLTC Details` existing_details ON existing_ltc.name = existing_details.parent
+                WHERE existing_ltc.docstatus < 2
+                AND existing_ltc.fiscal_year = %s
+                AND existing_details.employee = b.employee
+            )
+			
+			ORDER BY b.branch
+			"""
+
+			
+			
+			entries = frappe.db.sql(query, (start, end,self.fiscal_year), as_dict=True)
+	
+			
+		
+			self.set('items', [])
+
+			
+			for entry in entries:
+			
+				entry.tax = get_salary_tax(entry.amount)
+				entry.basic_pay = entry.amount
+				entry.amount = entry.amount - entry.tax
+
+				
+				row = self.append('items', {})
+				row.update(entry)
+
+			if not entries:
+				frappe.msgprint("No eligible employees found for LTC", indicator="red")
+
+		except Exception as e:
+			frappe.log_error(f"Error in get_ltc_details: {str(e)}", "LTC Eligibility Error")
+			frappe.throw("Failed to fetch LTC details. Please check the error log.")
 
--- hrms/hr/doctype/travel_authorization/travel_authorization.js
+++ hrms/hr/doctype/travel_authorization/travel_authorization.js
@@ -15,18 +15,7 @@ frappe.ui.form.on("Travel Authorization", {
 	refresh(frm) {
 		frm.call("has_travel_claim").then((r) => {
 			if (!r.message.has_travel_claim) {
-				if (
-					frm.doc.docstatus === 1 &&
-					frappe.model.can_create("Travel Advance")
-				) {
-					frm.add_custom_button(
-						__("Advance"),
-						function () {
-							frm.events.make_travel_advance(frm);
-						},
-						__("Create"),
-					);
-				}
+				
 
 				if (
 					frm.doc.docstatus === 1 &&
@@ -85,20 +74,7 @@ frappe.ui.form.on("Travel Authorization", {
 		});
 	},
 
-	make_travel_advance: function (frm) {
-		let method = "hrms.hr.doctype.travel_advance.travel_advance.make_travel_advance";
-		return frappe.call({
-			method: method,
-			args: {
-				dt: frm.doc.doctype,
-				dn: frm.doc.name,
-			},
-			callback: function (r) {
-				var doclist = frappe.model.sync(r.message);
-				frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
-			},
-		});
-	},
+	
 
 	employee: function (frm) {
 		if (frm.doc.employee) 
