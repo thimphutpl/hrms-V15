5de4c8f8ac6f25d2d2c8ef1d9ad8c1c37a8cc6e7
Pemanorbu <pemanorbu132@gmail.com>
2026-01-15 14:42:41 +0600

    2026-15-01

--- hrms/hr/doctype/employee_checkin/employee_checkin.py
+++ hrms/hr/doctype/employee_checkin/employee_checkin.py
@@ -129,7 +129,6 @@ class EmployeeCheckin(Document):
 		)
 		if not assignment_locations:
 			return
-erp@AGRO2025
 
 		checkin_radius, latitude, longitude = frappe.db.get_value(
 			"Shift Location", assignment_locations[0], ["checkin_radius", "latitude", "longitude"]
