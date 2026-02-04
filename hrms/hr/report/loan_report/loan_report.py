9ddf137be7e3fd755bef0d66a43c663a9de136ce
pemanorbu <pemanorbu@thimphu.bt>
2026-02-04 20:46:21 +0600

    Loan Report

--- hrms/hr/report/loan_report/loan_report.py
+++ hrms/hr/report/loan_report/loan_report.py
@@ -43,15 +43,14 @@ def get_data(filters):
 
 	data = frappe.db.sql("""
 		select t1.employee, t3.employee_name, t3.passport_number, t1.designation,
-			t2.reference_type, t2.institution_name, t2.reference_number, t2.amount, t2.total_deductible_amount, t2.total_outstanding_amount,
-			t1.company, t1.cost_center, t1.branch, t1.department, t1.division, t1.section,
+			t2.reference_type,t2.reference_number, t2.amount, t2.total_deductible_amount, t2.total_outstanding_amount,
+			t1.company, t1.cost_center, t1.branch, t1.department,t1.section,
 			t1.fiscal_year, t1.month
 		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
 		where t1.docstatus = 1 {}
 		and t3.employee = t1.employee
 		and t2.parent = t1.name
 		and t2.parentfield = 'deductions'
-		and case when t2.institution_name = 'RICBL' then t2.reference_type like '%loan%' else 1 = 1 end
 		and exists
 			(select 1
 				from `tabSalary Component` sc
@@ -72,7 +71,7 @@ def get_conditions(filters):
 	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = {}".format(filters.get("fiscal_year"))
 	if filters.get("company"): conditions += " and t1.company = '{}'".format(filters.get("company"))
 	if filters.get("employee"): conditions += " and t1.employee = '{}'".format(filters.get("employee"))
-	if filters.get("bank"): conditions += "and t2.institution_name = '{0}'".format(filters.bank)
+	if filters.get("bank"): conditions += "and t2.financial_scheme = '{0}'".format(filters.bank)
 	if filters.get("cost_center"): conditions += " and exists(select 1 from `tabCost Center` cc where t1.cost_center = cc.name and (cc.parent_cost_center = '{0}' or cc.name = '{0}'))".format(filters.cost_center)
 
 	return conditions, filters
