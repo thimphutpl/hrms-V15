d908f218ffcfec95006a94f4b9bbfcfa8742c189
pemanorbu <pemanorbu132@gmail.com.bt>
2026-02-18 11:40:38 +0600

    Payroll

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -634,7 +634,7 @@ def has_record_permission(doc, user):
 	if not user: user = frappe.session.user
 	user_roles = frappe.get_roles(user)
 	
-	if "HR User" in user_roles or "HR Manager" in user_roles:
+	if "HR User" in user_roles or "HR Manager" in user_roles or "Auditor" in user_roles:
 		return True
 	else:
 		if frappe.db.exists("Employee", {"name":doc.employee, "user_id": user}):
