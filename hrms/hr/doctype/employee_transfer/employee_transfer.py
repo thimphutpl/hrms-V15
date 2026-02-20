6b8f549bb48edfa1b99e3656d9adca97e1c7a16c
pemanorbu <pemanorbu132@gmail.com.bt>
2026-02-20 18:03:51 +0600

    ET

--- hrms/hr/doctype/employee_transfer/employee_transfer.py
+++ hrms/hr/doctype/employee_transfer/employee_transfer.py
@@ -136,7 +136,7 @@ def get_permission_query_conditions(user):
 	if user == "Administrator":
 		return
 
-	if "HR User" in user_roles or "HR Manager" in user_roles:
+	if "HR User" in user_roles or "HR Manager" in user_roles or "Auditor" in user_roles:
 		return
 	
 	return """(
