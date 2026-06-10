5f4c3d3e0c2ad7f176785d18bd4c748d1ba38186
Karma <ktenzleo@gmail.com>
2026-06-10 17:37:01 +0600

    leave todays

--- hrms/hr/dashboard_chart_source/today_leave_by_department/today_leave_by_department.py
+++ hrms/hr/dashboard_chart_source/today_leave_by_department/today_leave_by_department.py
@@ -9,7 +9,7 @@ def get_data(chart_name=None, filters=None, from_date=None, to_date=None, time_i
 	data = frappe.db.sql(
 		"""
 		select
-			coalesce(emp.department, 'No Department') as department,
+			coalesce(emp.branch, 'No Branch') as branch,
 			count(distinct la.employee) as leave_count
 		from `tabLeave Application` la
 		inner join `tabEmployee` emp
@@ -19,7 +19,7 @@ def get_data(chart_name=None, filters=None, from_date=None, to_date=None, time_i
 			and la.status = 'Approved'
 			and la.from_date <= %(today_date)s
 			and la.to_date >= %(today_date)s
-		group by emp.department
+		group by emp.branch
 		order by leave_count desc
 		""",
 		{"today_date": today_date},
@@ -27,7 +27,7 @@ def get_data(chart_name=None, filters=None, from_date=None, to_date=None, time_i
 	)
 
 	return {
-		"labels": [d.department for d in data],
+		"labels": [d.branch for d in data],
 		"datasets": [
 			{
 				"name": "On Leave Today",
