806a99b657a6c6f6975c308a397df838748c5ebf
Rinzin <rinzinnorbu@gyalsunginfra.bt>
2026-09-04 11:32:57 +0600

    updated

--- hrms/hr/doctype/arm_force/arm_force.py
+++ hrms/hr/doctype/arm_force/arm_force.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ArmForce(Document):
-	pass
+	def validate(self):
+		self.check_cid()
+	
+	def check_cid(self):
+		old_cid=frappe.db.get_value("Arm Force",{"cid_no":self.cid_no},"name")
+		if old_cid and old_cid != self.name:
+			frappe.throw("This Arm Force with same CID already registered in "+ old_cid)
