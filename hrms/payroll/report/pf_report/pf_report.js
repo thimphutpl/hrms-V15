d8c9a19906e891114494293c73f1340b61e1e7c5
SangaTenzin <sangatenzin101@gmail.com>
2026-09-08 16:02:57 +0600

    pf

--- hrms/payroll/report/pf_report/pf_report.js
+++ hrms/payroll/report/pf_report/pf_report.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
+// For license information, please see license.txt/
 /* eslint-disable */
 
 frappe.query_reports["PF Report"] = {
